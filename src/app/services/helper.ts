05073c997b1a4e65714a196d675c818f220174c7
soumikroy07 <85670300+soumikroy07@users.noreply.github.com>
2023-06-12 13:47:12 +0530

    change baseURL

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,3 +1,4 @@
 // let baseUrl = 'http://localhost:8080';
-let baseUrl = 'http://examportalbysoumik.info:8080';
+// let baseUrl = 'http://examportalbysoumik.info:8080';
+let baseUrl = 'https://api.examportalbysoumik.info';
 export default baseUrl;
